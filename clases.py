2c294ba096ee97970415300135009c6b62509b28
Isaac <isaacarayabusiness@hotmail.com>
2018-05-08 20:24:02 -0600

    Añadí archivo clases.py y una imagen

---
+++ clases.py
@@ -0,0 +1,16 @@
+#Clases
+
+#Barra
+#Atributos:
+# Tamaño: tuple
+# Velocidad: int
+# Posición: tuple
+#Métodos:
+# GetPos()
+# SetPos()
+# GetSpeed()
+# SetSpeed()
+# GerColor()
+# SetColor()
+# GetTamaño()
+# MoverBarra()
--- codigo.py
+++ codigo.py
@@ -2,6 +2,7 @@ import pygame
 import random
 import time
 
+
 pygame.init()
 fps= pygame.time.Clock()
 Width= 800
@@ -17,18 +18,31 @@ Red= (174, 24, 24)
 Green= (2, 184, 5)
 Yellow= (250, 195, 15)
 
+Menu= pygame.image.load("Menu.jpg")
+
+def mainMenu():
+    Inicio= True
+    while Inicio:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+        root.blit(Menu,(0,0))
+        pygame.display.update()
+        fps.tick()
 
 def mainloop():
     Winner = False
     while not Winner:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                Winner = True
+                pygame.quit()
+                quit()
         root.fill(Blue)
         pygame.display.update()
         fps.tick(60)
 
-
+mainMenu()
 mainloop()
 pygame.quit()
 quit()
