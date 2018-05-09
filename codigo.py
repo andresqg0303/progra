2cdc7ea873e3c83283064f198eb0cef31efdfb87
Isaac <isaacarayabusiness@hotmail.com>
2018-05-08 22:15:20 -0600

    Añadí funcion de botones a codigo.py

--- codigo.py
+++ codigo.py
@@ -9,16 +9,39 @@ Width= 800
 Height= 500
 
 root= pygame.display.set_mode((Width,Height))
+pygame.display.set_caption("Pong")
+
 
 Black= (0, 0, 0)
 White= (255, 255, 255)
+Grey= (130, 130, 130)
+LightGrey= (200,200,200)
 Blue= (29, 49, 86)
+LightBlue=  (49, 69, 106)
 Orange= (255, 139, 54)
 Red= (174, 24, 24)
 Green= (2, 184, 5)
 Yellow= (250, 195, 15)
 
 Menu= pygame.image.load("Menu.jpg")
+
+def ObjetosTexto(text, font):
+    SuperficieTexto = font.render(text, True, Black)
+    return SuperficieTexto, SuperficieTexto.get_rect()
+
+def Boton(msg,x,y,w,h,ic,ac):
+    mouse = pygame.mouse.get_pos()
+
+    if x+w > mouse[0] > x and y+h > mouse[1] > y:
+        pygame.draw.rect(root, ac,(x,y,w,h))
+    else:
+        pygame.draw.rect(root, ic,(x,y,w,h))
+
+    smallText = pygame.font.Font("freesansbold.ttf",20)
+    textSurf, textRect = ObjetosTexto(msg, smallText)
+    textRect.center = ( (x+(w/2)), (y+(h/2)) )
+    root.blit(textSurf, textRect)
+
 
 def mainMenu():
     Inicio= True
@@ -27,7 +50,11 @@ def mainMenu():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
-        root.blit(Menu,(0,0))
+                sys.exit()
+        root.blit(Menu, (0, 0))
+        #Boton(msg,x,y,w,h,ic,ac)
+        Boton("PVP",355,240,100,50,LightGrey, Grey)
+        Boton("PVC", 355, 320, 100, 50, LightGrey, Grey)
         pygame.display.update()
         fps.tick()
 
@@ -41,8 +68,6 @@ def mainloop():
         root.fill(Blue)
         pygame.display.update()
         fps.tick(60)
-
 mainMenu()
 mainloop()
-pygame.quit()
-quit()
+
