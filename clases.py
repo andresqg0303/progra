2202ff3500816420d4d474875481e20fa66bb879
Andrés <aandresqg03@gmail.com>
2018-05-08 20:47:23 -0600

    Clase bola

--- clases.py
+++ clases.py
@@ -36,6 +36,79 @@ class Barra:
 
     def GetTamaño(self):
         return self.tamaño = tamaño
+
+    def MoverBarra(self):
+        #Definir
+
+
+#___________________________________________________________
+
+#Bola
+
+#Atributos:
+# Color: tuple
+# Tamaño: tuple
+# Velocidad: int
+# Posición: tuple
+#Métodos:
+# GetPos()
+# GetSpeed()
+# GerColor()
+# GetTamaño()
+# Mover()
+# Dirección()
+
+class Bola:
+    color= ''
+    tamaño= ''
+    velocidad= 20
+    posicion= ''
+
+    def __init__(self, color,tamaño,velocidad,posicion):
+        self.color = color
+        self.tamaño = tamaño
+        self.velocidad = velocidad
+        self.posicion = posicion
+
+    def GetColor(self):
+        return self.color=color
+    
+    def GetSpeed(self):
+        return self.velocidad = velocidad
+
+    def GetPos(self):
+        return self.posición = posición
+
+    def GetTamaño(self):
+        return self.tamaño = tamaño
+
+    def Mover(self):
+        #Definir
+
+    def Dirección(self):
+        #Definir
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
     
 
     
